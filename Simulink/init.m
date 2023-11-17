b0cee6bd136487a7030258cd1fc6cbf921e037f0
solnor <solve.norsterud@gmail.com>
2023-11-17 23:34:39 +0100

    Add CDPR simulation with motor model

--- Simulink/init.m
+++ Simulink/init.m
@@ -22,21 +22,21 @@ x_ss_0 = [x(1); 0; x(2); 0; x(3);0];
 m = 4;
 n = 3;
 
-l   = inverseKinematics(a, b, x, m)
-u   = cableUnitVectors(a, b, x, l, m);
+l_0   = inverseKinematics(a, b, x, m)
+u   = cableUnitVectors(a, b, x, l_0, m);
 A_T = structureMatrix(u, x(3), b, m);
 A_T = A_T(2:4,:)
 m_p = 0.1;
 w = [0;-9.81*m_p;0];
 
-f_min = 10*ones(m,1); %[N]
-f_max = 200*ones(m,1); %[N]
+f_min = 2*ones(m,1); %[N]
+f_max = 60*ones(m,1); %[N]
 
 f_ref = 0.5*(f_min+f_max)
 
-f_0 = 20*ones(4,1);
+f_0 = 15*ones(4,1);
 
-k_spring = diag(2000*ones(m,1))
+k_spring = diag(200*ones(m,1))
 
 k_spring_inv = inv(k_spring);
 
@@ -71,7 +71,16 @@ B_p_d = [-0.2  0    0;
 
 
 r_winch = 37.5e-3;
-psi = l/r_winch
+r_winch = 60.0e-3;
+psi = l_0/r_winch
+l_0(1)
+f_0(1)
+k_spring(1,1)
+l_0(1)-f_0(1)*k_spring_inv(1,1)
+l_0(1)-f_0(1)*k_spring_inv(1,1)
+theta_r_0 = (l_0(1)-f_0(1)*k_spring_inv(1,1))/r_winch;
+l_0(1)-f_ref(1)*k_spring_inv(1,1)
+theta_r_0 = (l_0(1)-f_ref(1)*k_spring_inv(1,1))/r_winch;
 
 
 % MarLev position estimation constants
@@ -79,3 +88,5 @@ tau       = 1e-6;
 epsilon_1 = 1e-17;
 epsilon_2 = 1e-17;
 xi        = 2;
+
+motor_init;
--- Simulink/motor_init.m
+++ Simulink/motor_init.m
@@ -1,16 +1,16 @@
 % Mechanical constants
 J_m = 2.4059e-2;           % [kgm^2] - Moment of inertia of rotor
-J_m_inv = 1000*1/J_m;           %
+J_m_inv = 3000*1/J_m;           %
 n_phases = 3;              % Motor winding phases
 n_poles = 14;              % Number of poles in the stator
 n_pole_pairs = n_poles/2;  % Number of pole pairs in the stator
 
 % Electric constants
-R_m   = 19.00e-3; % [mOhm] - Stator resistance
-% R_m   = 0.04068; % [mOhm] - Stator resistance
-L_m   = 8.97e-6;  % [uH]   - Stator self inductance
-% L_m   = 40.43e-6;  % [uH]   - Stator self inductance
-psi_m = 4.88e-3;  % [mWb]  - Magnetic flux linkage between rotor magnets/stator coils
+R_m   = 19.00e-3; % [Ohm] - Stator resistance
+% R_m   = 0.04068; % [Ohm] - Stator resistance
+L_m   = 8.97e-6;  % [H]   - Stator self inductance
+% L_m   = 40.43e-6;  % [H]   - Stator self inductance
+psi_m = 4.88e-3;  % [Wb]  - Magnetic flux linkage between rotor magnets/stator coils
 
 % https://ieeexplore.ieee.org/mediastore_new/IEEE/content/media/63/7394209/6825903/liu.t4-2328655-large.gif
 % R_m = 1.5e-2;
@@ -21,7 +21,7 @@ K_e = 0.052;
 
 R_m_mat = R_m*eye(n_phases);
 L_m_mat = L_m*eye(n_phases);
-L_m_mat_inv = inv(L_m_mat);%*1e-4
+L_m_mat_inv = inv(L_m_mat);
 
 fourier_coeff_a = [ 0.08333    0.00000     0.00000;
                     0.00000    1.05296    -1.05296;
@@ -66,11 +66,4 @@ fourier_coeff_b = [-1.23020    0.60792     0.60792;
 
 % Clarke Transformation
 Clarke = 2/3*[1 -1/2       -1/2;
-              0 sqrt(3)/2  -sqrt(3)/2]
-
-test = [ 0.6667;
-        -0.3333;
-        -0.3333];
-test = [-0.3333;
-    0.6667;
-   -0.3333];
+              0 sqrt(3)/2  -sqrt(3)/2];
